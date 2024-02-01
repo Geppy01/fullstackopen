e7cf86d17cb84614328bbb4bb7eb2f92388fc830
Geppy <giuseppemonaco15@hotmail.com>
2024-02-01 10:42:08 +0100

    1.14 anecdotes step3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -6,6 +6,35 @@ const Button = (props) => {
   )
 }
 
+/*
+//First idea for doing last exercise but i ended up changing it because we didn't see the useEffect method yet.
+
+const DisplayMostVotedAnecdote = (props) =>{
+  let voteValuesCopy = [...props.valuesVote]
+  let anecdoteValuesCopy = [...props.valuesAnecdote]
+  let largest = props.valueMostVotedAnecdote
+  console.log(largest)
+  let indexBiggestAnecdoteVote = 0
+  for(let i=0; i<voteValuesCopy.length; i++){
+    if(voteValuesCopy[i]>largest){
+      largest = voteValuesCopy[i]
+      console.log(largest)
+      indexBiggestAnecdoteVote = i
+      console.log(indexBiggestAnecdoteVote)
+    }
+  }
+  props.setValueMostVotedAnecdote(largest)
+  return(
+    <>
+      <p>
+      {anecdoteValuesCopy[indexBiggestAnecdoteVote]}<br/>
+      has {largest} votes
+      </p>
+    </>
+  )
+}
+*/
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -21,6 +50,9 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   //Defining and initializing an array with the same length of anecdotes but filling it with all the values set to 0
   const [anecdotesVotes, setAnecdotesVotes] = useState(Array(anecdotes.length).fill(0))
+  /*Array of 2 that containts the value of biggest vote in index 0 and  the value of the index that equals to the
+    most voted anectode in index 1*/
+  const [mostVotedAnecdote, setMostVotedAnecdote] = useState(Array(2).fill(0))
 
   const handleRandomClick = () =>{
     let randomValue = Math.floor(Math.random() * 8)
@@ -31,14 +63,39 @@ const App = () => {
     let updatedVoteValues = [...anecdotesVotes]
     updatedVoteValues[selected] +=1
     setAnecdotesVotes(updatedVoteValues)
+    updateMostVotedAnecdote(updatedVoteValues)
+  }
+
+  const updateMostVotedAnecdote = (props) =>{
+    let voteValuesCopy = [...props]
+    let largestAndIndex = [...mostVotedAnecdote]
+
+    //for every item in voteValueCopy (the array that contains the copy of votes)
+    for(let i=0; i<voteValuesCopy.length; i++){
+      //if the updated vote in index i is greater than the vote in the biggest actual vote
+      if(voteValuesCopy[i]>largestAndIndex[0]){
+        //then copy in that position the value of vote (we have a new biggest vote)
+        largestAndIndex[0] = voteValuesCopy[i]
+        //and copy in the next position the index that equals to the most voted anectode       
+        largestAndIndex[1] = i
+      }
+    }
+    /*after cycling the entire array of votes we'll find out what is the most voted anectode and how many votes it has.
+      So we are updating MostVotedAnectode with the array that contains the number of votes in the first place and the index of the
+      anectode that has those votes in the second place*/
+    setMostVotedAnecdote(largestAndIndex)
   }
   
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <p>has {anecdotesVotes[selected]} votes</p>
       <Button onClick={handleRandomClick} text="next anecdote" />
       <Button onClick={handleVoteClick} text="vote" />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[mostVotedAnecdote[1]]}
+      <p>has {mostVotedAnecdote[0]} votes</p>
     </div>
   )
 }
